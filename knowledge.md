f5d7b620366524b84b90cbb0d0ba6a4e8e672151
lixu <lx507714@163.com>
2018-09-06 12:06:20 +0800

    methods方法

--- knowledge.md
+++ knowledge.md
@@ -43,5 +43,53 @@ v-if相当于是remove
 		});
 	</script>
 
-
 ```
+
+(5)、v-model表单输入绑定
+```
+<input v-model="message" placeholder="edit me">
+<p>Message is: {{ message }}</p>
+```
+(6)、methods,watch和computed的关系.
+```
+methods,watch和computed都是以函数为基础的，但各自却都不同 .
+1、methods里面定义的函数，是需要主动调用的，而和watch和computed相关的函数，会自动调用,完成我们希望完成的作用。
+作用机制：
+1.watch和computed都是以Vue的依赖追踪机制为基础的，它们都试图处理这样一件事情：当某一个数据（称它为依赖数据）发生变化的时候，所有依赖这个数据的“相关”数据“自动”发生变化，也就是自动调用相关的函数去实现数据的变动。
+
+2.对methods:methods里面是用来定义函数的，很显然，它需要手动调用才能执行。而不像watch和computed那样，“自动执行”预先定义的函数。
+
+从性质上看
+
+1.methods里面定义的是函数，你显然需要像"fuc()"这样去调用它（假设函数为fuc）
+2.computed是计算属性，事实上和和data对象里的数据属性是同一类的（使用上）,
+1.watch擅长处理的场景：一个数据影响多个数据
+2.computed擅长处理的场景：一个数据受多个数据影响。
+methods不处理数据逻辑关系，只提供可调用的函数
+利用computed处理methods存在的重复计算情况
+new Vue({
+ el: '#app',
+ // 设置两个button，点击分别调用getMethodsDate,getComputedDate方法
+ template: '<div id="app"><button @click="getMethodsDate">methods</button><button @click="getComputedDate">computed</button></div>',
+ methods: {
+  getMethodsDate: function () {
+   alert(new Date())
+  },
+  // 返回computed选项中设置的计算属性——computedDate
+  getComputedDate: function () {
+   alert(this.computedDate)
+  }
+ },
+ computed: {
+  computedDate: function () {
+   return new Date()
+  }
+ }
+ 【注意】为什么两次点击computed返回的时间是相同的呢？new Date()不是依赖型数据（不是放在data等对象下的实例数据），所以computed只提供了缓存的值，而没有重新计算
+ 
+  只有符合：1.存在依赖型数据 2.依赖型数据发生改变这两个条件,computed才会重新计算。 
+ 
+ 而methods下的数据，是每次都会进行计算的 
+```
+
+
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,33 +1,52 @@
 <template>
   <div>
+    <!--if...else-->
     <p v-if="show">you can see me</p>
     <p v-else>now you see me</p>
     <template v-if="show">
       <h1>heading</h1>
     </template>
 
+    <!--click-->
     <p v-show="show">do you also see me?</p>
     <button @click="show=!show">switch</button>
     <button v-on:click="show=!show">switch</button>
 
+
+    <!--html-->
     <p>{{sayHello()}} - <a v-bind:href="link">Google</a></p>
     <p v-html="finishLink"></p>
 
+
+    <!--v-once-->
     <p v-once>{{title}}</p>
     <p>{{title}}</p>
     <input type="text" v-model = "title" name="">
 
+
+    <!--for循环-->
     <ul>
       <li v-for="(ingredient, i) in ingredients" :key="ingredient">
        ({{i}})、 {{ingredient}}
       </li>
     </ul>
 
-    <button v-on:click="increase(2)">Click me</button>
-    <p>{{count}}</p>
-    <p v-on:mousemove="updateCoordinates">Coordinates: {{x}} / {{y}} -
-      <!--<span v-on:mousemove.stop>DEAD SPOT</span>-->
+
+    <!--watch methods  computed-->
+    <button v-on:click="increase">Click me</button>
+    <button v-on:click="count++">Click me</button>
+    <p>{{count*2 >10 ?'Greater than 10':'Smaller than 10'}}</p>
+    <p>count:{{count}}</p>
+    <p>result:{{result()}} | {{output}}</p>
+    <p v-on:mousemove="updateCoordinates($event)">Coordinates: {{x}} / {{y}} -
+      <span v-on:mousemove.stop="">DEAD SPOT</span>
     </p>
+
+
+    <!--绑定-->
+    <input type="text" v-on:keyup.enter.space="alertMe">
+    <input type="text" v-model="name">
+    <p>{{name}}</p>
   </div>
 
 </template>
@@ -38,6 +57,7 @@
     component:{HelloWorld:HelloWorld},
     data(){
      return {
+       name:'李旭',
        x:0,
        y:0,
        count:0,
@@ -66,13 +86,32 @@
         this.title = '33'+ Math.random();
         return this.title;
       },
-      increase:function(step) {
-        this.count +=step;
+      result() {
+        return this.count > 5 ? 'Greater 5':"Smaller than 5";
       },
-      updateCoordinates(event) {
+      increase:function() {
+        this.count +=1;
+      },
+      updateCoordinates:function(event) {
 //        console.log(event);
 //        this.x= event.clientX;
 //        this.y= event.clientY;
+      },
+      alertMe:function() {
+        alert('Alert!');
+      }
+    },
+    computed:{
+      output:function() {
+        return this.count > 5 ? 'Greater 5':"Smaller than 5";
+      }
+    },
+    watch:{
+      count:function (value) {
+        var vm = this;
+        setTimeout(function() {
+          vm.count = 0;
+        },2000)
       }
     }
   }
